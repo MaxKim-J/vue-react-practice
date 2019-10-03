4ca16a430ff0aa26a469b9bbac5618bd92b9dd29
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-10-03 20:36:33 +0900

    Restful API로 서버와 연동 구현

--- practice1-memo/src/components/MemoApp.vue
+++ practice1-memo/src/components/MemoApp.vue
@@ -16,6 +16,11 @@
 <script>
 import MemoForm from './MemoForm';
 import Memo from './Memo';
+import axios from 'axios';
+
+const memoAPICore = axios.create({
+	baseURL: 'http://localhost:8000/api/memos',
+});
 
 export default {
 	name: 'MemoApp',
@@ -33,30 +38,43 @@ export default {
 			const { id, content } = payload;
 			const targetIndex = this.memos.findIndex(v => v.id === id);
 			const targetMemo = this.memos[targetIndex];
-			this.memos.splice(targetIndex, 1, { targetMemo, content });
-			this.storeMemo();
+			memoAPICore.put(`/${id}`, { content }).then(() => {
+				this.memos.splice(targetIndex, 1, { targetMemo, content });
+			});
 		},
 		deleteMemo(id) {
 			const targetIndex = this.memos.findIndex(v => v.id === id);
-			this.memos.splice(targetIndex, 1);
-			this.storeMemo();
+			// 특정 메모 객체애 대한 삭제요청
+			memoAPICore.delete(`/${id}`).then(() => {
+				// 요청 때린 후 memo에서도 삭제
+				this.memos.splice(targetIndex, 1);
+			});
 		},
 		addMemo(payload) {
-			// 올려받은 데이터를 먼저 컴포넌트 내부 데이터에 추가
-			this.memos.push(payload);
+			// 에이피아이로 올려받은 데이터 post방식 전달
+			memoAPICore.post('/', payload).then(res => {
+				// 결과값 memos에 푸시
+				this.memo.push(res.data);
+			});
 			// 내부 데이터를 문자열로 변환하고, 로컬 스토리지에 저장함
 			this.storeMemo();
 		},
-		storeMemo() {
-			const memosToString = JSON.stringify(this.memos);
-			localStorage.setItem('memos', memosToString);
-		},
+		// api에서는 로컬 스토리지 사용하지 않음
+		// storeMemo() {
+		// 	const memosToString = JSON.stringify(this.memos);
+		// 	localStorage.setItem('memos', memosToString);
+		// },
 	},
 	// created 훅 - 삼항 연산자를 통해 로컬스토리지의 데이터 memos를 초기화함
 	// 컴포넌트의 초기화에 필요한 데이터를 외부 app에서 요청해서 받아와야 하는 경우
 	// 실행 타이밍 가장 빠른 created 훅에서 데이터를 받아옴
 	created() {
-		this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+		// api를 사용할때는 로컬스토리지를 사용할 필요가 없다
+		// this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+		memoAPICore.get('/').then(res => {
+			//받아온 데이터를 data의 memos에 저장한다
+			this.memos = res.data;
+		});
 	},
 };
 </script>
