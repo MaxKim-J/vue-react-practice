50cd3ac035124d842e0d25818eb3119c017f5a80
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-10-07 03:26:00 +0900

    suggestTip component template

--- practice2-prac/.eslintrc.js
+++ practice2-prac/.eslintrc.js
@@ -1,14 +1,33 @@
+
 module.exports = {
+  // 현재 eslintrc 파일을 기준으로 ESLint 규칙을 적용
   root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  // 추가적인 규칙들을 적용
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  // 코드 정리 플러그인 추가
+  plugins: ['prettier'],
+  // 사용자 편의 규칙 추가
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'prettier/prettier': [
+      'error',
+      // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+      // https://prettier.io/docs/en/options.html
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
 };
---
+++ practice2-prac/src/components/SuggestTip.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="suggestWrapper">
+		<div class="suggestCol1">
+			<div class="suggestRect"></div>
+		</div>
+		<div class="suggestCol2">
+			<div class="suggestTitle">나만의 리마인드 레터링 만들기</div>
+			<div class="suggestContent">
+				나만의 의미 담긴 글로 공간 분위기를 확 바꾸는 마법같은 순간을
+				맞이하세요!
+			</div>
+			<div class="suggestPurchase">지금 바로 구매하기</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SuggestTip',
+};
+</script>
+
+<style lang="scss">
+.suggestWrapper {
+	width: 100%;
+	height: 240px;
+	background-color: #f1f8ff;
+	display: flex;
+	flex-direction: row;
+	align-content: flex-start;
+}
+
+.suggestCol1 {
+	width: 30%;
+	.suggestRect {
+		width: 110px;
+		height: 110px;
+		background-color: #d8d8d8;
+		margin: 60px 0px 70px 29px;
+	}
+}
+.suggestCol2 {
+	width: 50%;
+	display: block;
+	.suggestTitle {
+		margin-top: 51px;
+		font-weight: 500;
+		font-size: 24px;
+	}
+	.suggestContent {
+		height: 72px;
+		font-weight: 300;
+	}
+	.suggestPurchase {
+		margin-top: 1px;
+		font-weight: 300;
+	}
+}
+</style>
